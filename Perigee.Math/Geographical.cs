e83157c57c62d64ca0e542f7dd8c90415b507609
Stephen York <steve@steve-york.com>
2023-09-14 09:56:13 +1000

    feat: better implementation

--- Perigee.Math/Geographical.cs
+++ Perigee.Math/Geographical.cs
@@ -1,9 +1,9 @@
 ﻿namespace Perigee;
 
-public static class Geographical
+public class Geographical
 {
     // Code lifted from https://stormconsultancy.co.uk/blog/storm-news/the-haversine-formula-in-c-and-sql/
-    public static double HaversineDistance(LatLng pos1, LatLng pos2, DistanceUnit unit)
+    public static double HaversineDistance(Location pos1, Location pos2, DistanceUnit unit)
     {
         double R = (unit == DistanceUnit.Miles) ? 3960 : 6371;
         var lat = (pos2.Latitude - pos1.Latitude).ToRadians();
@@ -17,26 +17,38 @@ public static class Geographical
 
     public enum DistanceUnit { Miles, Kilometers };
 
-    public class LatLng
+    public class Location
     {
         public double Latitude { get; set; }
         public double Longitude { get; set; }
 
-        public LatLng()
-        {
-        }
-
-        public LatLng(double lat, double lng)
+        public Location(double lat, double lng)
         {
             this.Latitude = lat;
             this.Longitude = lng;
         }
     }
 
+    public static double? GetDistance(double fromLat, double fromLong, double toLat, double toLong)
+    {
+        if (fromLat == 0 || fromLong == 0 || toLat == 0 || toLong == 0)
+            return null;
+
+        var distance = Geographical.HaversineDistance(
+            new Geographical.Location(fromLat, fromLong),
+            new Geographical.Location(toLat, toLong),
+            Geographical.DistanceUnit.Kilometers);
+
+        return distance;
+    }
+
+}
+
+internal static class GeographicalExtensions
+{
     public static double ToRadians(this double degrees)
     {
         double radians = (Math.PI / 180) * degrees;
         return (radians);
     }
-
 }
